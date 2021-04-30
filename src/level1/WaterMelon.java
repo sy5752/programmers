776a7da173fa7c6d208a0f45fc0c916d41e33903
sy5752 <psy5752@gmail.com>
2021-04-30 09:15:06 +0900

    level1_수박수박

--- src/level1/WaterMelon.java
+++ src/level1/WaterMelon.java
@@ -3,18 +3,18 @@ package level1;
 public class WaterMelon {
 
 	public static void main(String[] args) {
-		
+
 		int n = 4;
 		String answer = "";
-			for(int i=0; i<n; i++) {
-				if(i%2 == 0) {
-					answer += "수";
-			}else {
+		
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0) {
+				answer += "수";
+			} else {
 				answer += "박";
-				}
 			}
-			System.out.println(answer);;
-		}  
-	       
+		}
+		System.out.println(answer);
 	}
 
+}
